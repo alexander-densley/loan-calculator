ea141cf38d1c82ac84a171636301f0128b7e3f8c
Alexander Densley <alexander.densley@byu.edu>
2022-07-26 11:12:25 -0600

    added calculations and error alert

--- app.js
+++ app.js
@@ -19,8 +19,31 @@ function calculateResults(e){
     const monthlyPayment = (principle*x*calculatedInterest)/(x-1)
 
     if(isFinite(monthlyPayment)){
-        
+        monPay.value = monthlyPayment.toFixed(2)
+        totalPay.value = (monthlyPayment * calculatedPayments).toFixed(2);
+        totInt.value = ((monthlyPayment * calculatedPayments) - principle).toFixed(2);
+    }
+    else {
+        showError('Please check your numbers')
     }
 
     e.preventDefault()
+}
+
+function showError(error){
+    const errDiv = document.createElement('div');
+    
+    const card = document.querySelector('.card')
+    const heading = document.querySelector('.heading')
+    
+    errDiv.className = 'alert alert-danger'
+    errDiv.appendChild(document.createTextNode(error))
+
+    card.insertBefore(errDiv, heading)
+    
+    setTimeout(clearError, 3000)
+}
+
+function clearError(){
+    document.querySelector('.alert').remove();
 }
