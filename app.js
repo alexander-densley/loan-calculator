d22e9b396e7794cf37e416c0d4198c51bfd1675f
Alexander Densley <alexander.densley@byu.edu>
2022-07-28 10:17:37 -0600

    fixed when error function so the wheel doesnt spin forever

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 //Listen for submit
 
-document.getElementById('loan-form').addEventListener('submit', calculateResults)
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    document.getElementById('results').style.display = 'none'
+    document.getElementById('loading').style.display = 'block'
+    setTimeout(calculateResults, 2000)
 
-function calculateResults(e){
+    e.preventDefault();
+})
+
+function calculateResults(){
     console.log('Calculationg')
     const amount = document.getElementById('amount')
     const interest = document.getElementById('interest')
@@ -22,15 +28,21 @@ function calculateResults(e){
         monPay.value = monthlyPayment.toFixed(2)
         totalPay.value = (monthlyPayment * calculatedPayments).toFixed(2);
         totInt.value = ((monthlyPayment * calculatedPayments) - principle).toFixed(2);
+        document.getElementById('results').style.display = 'block'
+        document.getElementById('loading').style.display = 'none'
+
+
     }
     else {
         showError('Please check your numbers')
     }
 
-    e.preventDefault()
 }
 
 function showError(error){
+
+    document.getElementById('results').style.display = 'none'
+    document.getElementById('loading').style.display = 'none'
     const errDiv = document.createElement('div');
     
     const card = document.querySelector('.card')
